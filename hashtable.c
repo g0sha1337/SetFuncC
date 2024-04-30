9085a52455b6651d5480f3f76894d70c9b0b016e
g0sha1337 <yabloonchk@gmail.com>
2024-05-01 00:23:11 +0300

    add some func

--- hashtable.c
+++ hashtable.c
@@ -1,5 +1,5 @@
 #include "hashtable.h"
-
+#include <string.h>
 
 Data* InitData(DataType type) {
 	Data* data = (Data*)malloc(sizeof(Data));
@@ -10,7 +10,61 @@ Data* InitData(DataType type) {
 	
 	return data;
 }
-void FreeData(Data* data){
+void FreeData(Data* data) {
 	if (data->type == CHAR_TYPE) free(data->Char);
 	free(data);
 }
+
+HtItem* CreateItem (char* key, char* value){
+	HtItem* item = (HtItem*) malloc(sizeof(HtItem));
+	item->key = (char*)malloc(strlen(key)+1);
+	item->value = (char*) malloc(strlen(value)+1);
+	strcpy(item->key, key);
+	strcpy(item->value, value);
+	return item;
+}
+
+HashTable* CreateTable(int size){
+	HashTable* table = (HashTable*)malloc(sizeof(HashTable));
+	table->size = size;
+	table->count = 0;
+	table->items = (HtItem**) calloc(table->size, sizeof(HtItem*));
+
+	for (int i =0; i<talbe->size; i++){
+		table->items[i] = NULL;
+
+	}
+	return table;
+}
+
+void FreeItem(HtItem* item){
+	free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void FreeTable(HashTable* table){
+	for (int i=0; i<table->size; i++){
+		HtItem* item = table->items[i];
+		if (item!=0) FreeItem(item);
+	}
+	free(table->items);
+	free(table);
+}
+
+
+void PrintTable(HashTable* ht)
+{
+    printf("\nHash Table\n-------------------\n");
+
+    for (int i = 0; i < ht->size; i++)
+    {
+        if (ht->items[i])
+        {
+            printf("Index:%d, Key:%s, Value:%s\n", i, ht->items[i] -> key, ht->items[i]->value);
+        }
+    }
+
+    printf("-------------------\n\n");
+}
+
--- hashtable.h
+++ hashtable.h
@@ -1,6 +1,7 @@
 #pragma once
-#define HASHTABLE_SIZE 65536
+#define HASHTABLE_SIZE 65536 //2**16
 
+//Data
 typedef enum {
     INT_TYPE, CHAR_TYPE, FLOAT_TYPE
 } DataType;
@@ -12,5 +13,27 @@ typedef struct Data{
     float Float;
 } Data;
 
+
+//HashTable
+
+typedef struct HtItem{
+    char* key; 
+    char* value;
+} HtItem;
+
+typedef struct HashTable{
+    HtItem** items;
+    int size;
+    int count;
+} HashTable;
+
+HtItem* CreateItem(char* key, char* value);
+HashTable* CreateTable(int size);
+
+
+
 Data* InitData(DataType type);
 void FreeData(Data* data);
+void FreeItem(HtItem* item);
+void FreeTable(HashTable* table);
+void PrintTable(HashTable* ht);
